73781a6e26af5319bfac9218f28bd2c63431d87d
craig <craig.flewelling@gmail.com>
2023-06-26 17:50:37 +0000

    Made test paths relative to one repo_path.

--- usage.py
+++ usage.py
@@ -5,18 +5,27 @@ Created on Thu Jul 21 16:07:44 2022
 
 @author: craig
 """
+
+from pathlib import Path
+
 import sys
-# the mock-0.3.1 dir contains testcase.py, testutils.py & mock.py
-sys.path.append('/home/craig/workspace/compare_one-to-n/')
+if '__file__' in globals():
+    repo_path=str(Path(__file__).parent)
+else: 
+    repo_path='/home/craig/workspace/make-one-to-n'
+    
+repo_path=repo_path+'/'
+sys.path.append(repo_path)
+
 import core as core
 
-supp_demand_path = "/home/craig/workspace/compare_one-to-n/SupplyDemand.xlsx"
-interests_path = "/home/craig/workspace/compare_one-to-n/interesting_srcs.xlsx"
-run_dict = {core.variable_name : "/home/craig/workspace/compare_one-to-n/1-n.xlsx",
-      '20' : "/home/craig/workspace/compare_one-to-n/1-n.xlsx",
-      '25' : "/home/craig/workspace/compare_one-to-n/1-n.xlsx"}
-first_cuts_output='/home/craig/workspace/compare_one-to-n/first_cuts.xlsx'
-total_cuts_table='/home/craig/workspace/compare_one-to-n/total_cuts.xlsx'
+supp_demand_path = repo_path+"SupplyDemand.xlsx"
+interests_path = repo_path+"interesting_srcs.xlsx"
+run_dict = {core.variable_name : repo_path+"1-n.xlsx",
+      '20' : repo_path+"1-n.xlsx",
+      '25' : repo_path+"1-n.xlsx"}
+first_cuts_output=repo_path+'first_cuts.xlsx'
+total_cuts_table=repo_path+'total_cuts.xlsx'
 
 
 core.spit_cuts(run_dict, 
